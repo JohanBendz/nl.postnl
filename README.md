631d52148b3f562aad70af97a9f53c3124647ec3
Emile Nijssen <emile@athom.nl>
2018-01-26 16:34:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PostNL
+
+Track your shipments in Homey! Trigger a Flow when a parcel has been collected, sorted, is in transit or has been delivered.
+
+Someone pressed the doorbell while a parcel is underway? It's probably the mailman! Give them a warm welcome with your smart home, or automatically open the door when you're at home.
---
+++ app.js
@@ -0,0 +1,122 @@
+'use strict';
+
+const Homey = require('homey')
+const PostNLApi = require('./lib/PostNLApi.js')
+
+const POLL_INTERVAL = 1000 * 60 * 5; // 5 min
+
+class PostNLApp extends Homey.App {
+	
+	onInit() {
+		
+		// init flows
+		this._statusFlows = {};
+		
+		for( let i = 1; i <= 4; i++ ) {
+			let card = this._statusFlows[`status_${i}`] = new Homey.FlowCardTrigger(`status_${i}`);
+				card.register();
+		}
+		
+		// init postnl
+		this._pollInboxInterval = setInterval(this.pollInbox.bind(this), POLL_INTERVAL);
+		this.pollInbox();
+		
+		this.log('PostNLApp is running...');
+		
+	}
+	
+	async getUser() {
+		return this._getAuthenticatedApi()
+			.then( api => {
+				return api.getProfile()
+			})		
+	}
+	
+	async setUser( username, password ) {
+		
+		let api = new PostNLApi();
+		return api.getToken( username, password )
+			.then( token => {
+						
+				Homey.ManagerSettings.set('username', username);
+				Homey.ManagerSettings.set('password', password);
+				
+				return api.getProfile();
+			})
+	}
+	
+	async unsetUser() {
+		Homey.ManagerSettings.unset('username');
+		Homey.ManagerSettings.unset('password');
+	}
+	
+	async _getAuthenticatedApi() {
+		
+		let api = new PostNLApi();
+		
+		let username = Homey.ManagerSettings.get('username');
+		let password = Homey.ManagerSettings.get('password');
+				
+		if( !username || !password ) throw('missing_credentials');
+		
+		return api.getToken( username, password )
+			.then( token => {
+				return api;
+			})
+	}
+	
+	pollInbox() {
+		this._getAuthenticatedApi()
+			.then( api => {				
+				return api.getInbox();
+			}).then( inbox => {
+				
+				this.log(`Found ${inbox.receiver.length} shipments`);
+				
+				inbox.receiver.forEach(shipment => {
+					let key = shipment.key;
+					let barcode = shipment.trackedShipment.barcode
+					let senderTitle = ( shipment.sender && shipment.sender.companyName ) || ''
+					let statusTitle = shipment.delivery.status
+					let statusIndex = shipment.delivery.phase.index
+					let title = shipment.trackedShipment.title || senderTitle || barcode
+					
+					let settingKey = `shipment_${key}`;
+					let shouldSave = false;
+					let shouldTriggerFlow = false;
+					let setting = Homey.ManagerSettings.get(settingKey);
+					if( setting ) {
+						if( setting.statusIndex !== statusIndex ) {
+							shouldSave = true;
+							shouldTriggerFlow = true;
+						}
+					} else {
+						shouldSave = true;
+						shouldTriggerFlow = ( statusIndex < 4 )
+					}
+					
+					if( shouldSave ) {
+						Homey.ManagerSettings.set(settingKey, {
+							barcode: barcode,
+							statusIndex: statusIndex,
+							lastUpdated: new Date()
+						})
+					}
+					
+					if( shouldTriggerFlow ) {
+						this.log(`Triggering Flow for ${title} (${barcode})`)
+						this._statusFlows[`status_${statusIndex}`].trigger({
+							barcode: barcode,
+							title: title,
+						}).catch( this.error )
+					}
+				})
+			}).catch( err => {
+				this.error( err );
+			})
+		
+	}
+	
+}
+
+module.exports = PostNLApp;
